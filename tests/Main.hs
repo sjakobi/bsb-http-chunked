00f6b95da3ed8402b849e9fdd45b2c48e35dfd86
Simon Jakobi <simon.jakobi@gmail.com>
2018-03-17 20:45:35 +0100

    Add roundtrip test

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,15 +1,15 @@
-{-# language OverloadedStrings #-}
+{-# language OverloadedStrings, MultiWayIf #-}
 module Main where
 
+import Data.Attoparsec.ByteString.Char8 (Parser, (<?>))
+import qualified Data.Attoparsec.ByteString.Char8 as A
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
-import Data.ByteString.Builder (Builder)
 import qualified Data.ByteString.Builder as BB
 import Data.ByteString.Builder.HTTP.Chunked
-import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString.Lazy as BL
-import Data.Char
-import Data.List.NonEmpty (NonEmpty)
+import Data.Foldable
+import Data.Functor
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
@@ -18,41 +18,70 @@ import Test.Tasty.Hedgehog
 import Test.Tasty.HUnit (testCase, (@?=))
 
 main :: IO ()
-main = defaultMain properties
+main = defaultMain $ testGroup "Tests" [properties, unitTests]
 
 properties :: TestTree
 properties = testGroup "Properties"
-  [ p "Non-empty inputs get CRLF suffix" $ do
-      bss <- forAll $ genBSs
-      assert $ all ("\r\n" `BS.isSuffixOf`) (encode bss)
+  [ p "Encoding and decoding roundtrips" $ do
+      lbs <- forAll genLBS
+      tripping lbs
+               (BB.toLazyByteString . chunkedTransferEncoding . BB.lazyByteString)
+               parseTransferChunks
   ]
+  where
+    p name = testProperty name . property
 
-genPackedBS :: Gen ByteString
-genPackedBS = BS.pack <$> Gen.list (Range.linear 0 100) (Gen.word8 Range.constantBounded)
+genLBS :: Gen BL.ByteString
+genLBS = BL.fromChunks <$> genBSs
+
+genBSs :: Gen [ByteString]
+genBSs = Gen.list (Range.linear 0 5) genSnippedBS
 
 genSnippedBS :: Gen ByteString
 genSnippedBS = do
   d <- Gen.int (Range.linear 0 5)
-  t <- Gen.int (Range.linear 3 95)
-  BS.drop d . BS.take t <$> genPackedBS
+  e <- Gen.int (Range.linear 0 5)
+  BS.drop d . dropEnd e <$> genPackedBS
+  where
+    dropEnd n bs = BS.take m bs
+      where m = BS.length bs - n
 
-genBSs :: Gen (NonEmpty ByteString)
-genBSs = Gen.nonEmpty (Range.linear 1 10) genSnippedBS
+genPackedBS :: Gen ByteString
+genPackedBS = BS.pack <$> Gen.list (Range.linear 0 1000) (Gen.word8 Range.constantBounded)
 
-toBB :: Foldable t => t ByteString -> Builder
-toBB = foldMap BB.byteString
+parseTransferChunks :: BL.ByteString -> Either String BL.ByteString
+parseTransferChunks = fmap (BL.fromChunks . concat) .
+                      traverse (A.eitherResult . fmap toList . A.parse transferChunkParser) .
+                      BL.toChunks
 
-p :: TestName -> PropertyT IO () -> TestTree
-p name = testProperty name . property
+-- Adapted from snap-server
+transferChunkParser :: Parser (Maybe ByteString)
+transferChunkParser = parser <?> "encodedChunkParser"
+  where
+    parser = do
+        hex <- A.hexadecimal <?> "hexadecimal"
+        -- skipWhile (/= '\r') <?> "skipToEOL" -- We don't add chunk extensions
+        void crlf <?> "linefeed"
+        if | hex >= mAX_CHUNK_SIZE
+            -> fail $ "Chunk of size " ++ show hex ++
+                 " is too long. Max chunk size is " ++ show mAX_CHUNK_SIZE
+           | hex < 0
+             -> fail $ "Negative chunk size: " ++ show hex
+           | hex == 0
+             -> (crlf >> return Nothing) <?> "terminal crlf after 0 length"
+           | otherwise
+             -> do
+                x <- A.take hex <?> "reading data chunk"
+                void crlf <?> "linefeed after data chunk"
+                return $! Just x
+
+    -- Chunks larger than this may indicate denial-of-service attack.
+    mAX_CHUNK_SIZE = 2^(18::Int)
+
+    crlf = A.string "\r\n"
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
-  [ testCase "Encoding an empty BS returns an empty BS" $
-      encode [""] @?= []
+  [ testCase "Encoding an empty builder returns an empty builder" $
+      BB.toLazyByteString (chunkedTransferEncoding mempty) @?= ""
   ]
-
-encode :: Foldable t => t ByteString -> [ByteString]
-encode = BL.toChunks . BB.toLazyByteString . chunkedTransferEncoding . toBB
-
-encodedLength :: ByteString -> Int
-encodedLength = read . ("0x" ++) . C8.unpack . C8.takeWhile isHexDigit
