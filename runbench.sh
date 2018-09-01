c852a10c71a010ab64ed87b6d5e0973f10bc01ec
Simon Jakobi <simon.jakobi@gmail.com>
2018-09-01 20:57:34 +0200

    Fix runbench.sh

--- runbench.sh
+++ runbench.sh
@@ -2,4 +2,4 @@
 
 /opt/ghc/bin/cabal new-build --enable-benchmarks -O2 -w ghc-8.4.3
 
-/home/simon/src/bsb-http-chunked/dist-newstyle/build/x86_64-linux/ghc-8.4.3/bsb-http-chunked-0.0.0.2/b/bench/opt/build/bench/bench --measure-with /home/simon/src/bsb-http-chunked/dist-newstyle/build/x86_64-linux/ghc-8.4.3/bsb-http-chunked-0.0.0.3/b/bench/opt/build/bench/bench --small
+/home/simon/src/bsb-http-chunked/dist-newstyle/build/x86_64-linux/ghc-8.4.3/bsb-http-chunked-0.0.0.3/b/bench/opt/build/bench/bench --measure-with /home/simon/src/bsb-http-chunked/dist-newstyle/build/x86_64-linux/ghc-8.4.3/bsb-http-chunked-0.0.0.3/b/bench/opt/build/bench/bench --small
