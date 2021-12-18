b1d4d55215ff38743a3dd1171179058406a19077
Simon Jakobi <simon.jakobi@gmail.com>
2021-12-18 02:48:00 +0100

    Delete runbench.sh
    It's useless with the `tasty-bench`-based benchmarks

--- runbench.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/ghc/bin/cabal new-build --enable-benchmarks -O2 -w ghc-8.4.3
-
-/home/simon/src/bsb-http-chunked/dist-newstyle/build/x86_64-linux/ghc-8.4.3/bsb-http-chunked-0.0.0.3/b/bench/opt/build/bench/bench --measure-with /home/simon/src/bsb-http-chunked/dist-newstyle/build/x86_64-linux/ghc-8.4.3/bsb-http-chunked-0.0.0.3/b/bench/opt/build/bench/bench --small
