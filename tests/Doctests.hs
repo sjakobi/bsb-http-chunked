d0360ea678001161cc1a7590cefd25e9baadd395
Simon Jakobi <simon.jakobi@gmail.com>
2021-12-19 17:32:07 +0100

    Fix doctests for compatibility with GHC 9.2 (#42)
    Based on advice from @sol in
    https://github.com/sol/doctest/issues/326#issuecomment-997369942.

--- tests/Doctests.hs
+++ tests/Doctests.hs
@@ -1,6 +1,6 @@
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["-isrc", "Data/ByteString/Builder/HTTP/Chunked.hs"]
+main = doctest ["-isrc", "-XHaskell2010", "Data/ByteString/Builder/HTTP/Chunked.hs"]
 
 
